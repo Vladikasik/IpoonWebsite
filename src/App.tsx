0bee24f5081a5a292eba7db47130b1a14acf2e32
DocSkullka <docskullka@gmail.com>
2024-09-18 02:11:35 +0300

    11

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,8 @@ function App() {
         <meta property="og:title" content="Lightwork Labs"/>
         <meta property="og:description" content="Community driven consumer startup incubator"/>
         <meta property="og:image" content="https://for-vlad.vercel.app/assets/link-preview.png"/>
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="300" />
         <meta property="og:url" content={'https://for-vlad.vercel.app' + window.location.pathname + window.location.search}/>
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="og:site_name" content="Lightwork"/>
